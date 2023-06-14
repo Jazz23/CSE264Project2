dc0807a3b8385473cbd1ed9927a77b317dfa18e6
Jazz23 <justinfrauenhofer@gmail.com>
2023-06-14 09:39:19 -0700

    got matrix and error 0.26910541997656573

---
+++ calibration.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+import glob
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+images = glob.glob('chessboard/*.jpg')
+
+for fname in images:
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        print(fname)
+        objpoints.append(objp)
+
+        cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        imgpoints.append(corners)
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+# Save the mtx, dist, rvecs, tvecs to respective files
+np.save('mtx.npy', mtx)
+np.save('dist.npy', dist)
+np.save('rvecs.npy', rvecs)
+np.save('tvecs.npy', tvecs)
+
+tot_error = 0
+for i in range(len(objpoints)):
+    imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+    tot_error += error
+
+print("total error: ", tot_error / len(objpoints))
+
+
+cv2.destroyAllWindows()
